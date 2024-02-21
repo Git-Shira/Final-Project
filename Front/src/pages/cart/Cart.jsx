90349955517f4f1f26c3158ffe049cf6761bd568
shir babay <shirbabay1@gmail.com>
2024-02-21 22:58:36 +0200

    commit-shir

--- Front/src/pages/cart/Cart.jsx
+++ Front/src/pages/cart/Cart.jsx
@@ -6,66 +6,75 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { removeItem, editItem } from "../../redux/cartSlice";
-import { Dialog } from "@mui/material";
-import { Button } from "@mui/material";
-import { DialogActions } from "@mui/material";
-import { DialogContent } from "@mui/material";
-import { DialogContentText } from "@mui/material";
-import { DialogTitle } from "@mui/material";
-import { TextField } from "@mui/material";
-import { Typography } from "@mui/material";
-import { Box } from "@mui/material";
-import Cookies from "js-cookie";
+import Dialog from "@mui/material/Dialog";
+import Button from "@mui/material/Button";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
+
 export default function Cart() {
-  const [quantityArray, setQuantityArray] = React.useState([]);
-  const [allProducts, setAllProducts] = React.useState([]);
-  const [totalPrice, setTotalPrice] = React.useState(0);
-  const [open, setOpen] = React.useState(false);
-  const [quantity, setQuantity] = React.useState(0);
-  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [cartData, setCartData] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [selectedProduct, setSelectedProduct] = useState({});
+  const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
+  const cookieCart = Cookies.get("cart");
 
-  const handleOpenDialog = (product) => {
-    debugger;
-    setSelectedProduct(product);
+  useEffect(() => {
+    if (cookieCart) {
+      const parsedCart = JSON.parse(cookieCart);
+      setCartData(parsedCart);
+      updateTotalPrice(parsedCart);
+    }
+  }, [cookieCart]);
 
+  const updateTotalPrice = (cartItems) => {
+    const total = cartItems.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0
+    );
+    setTotalPrice(total);
+  };
+
+  const deleteItem = (id) => {
+    dispatch(removeItem(id));
+    handleCloseDialog();
+    const updatedCart = cartData.filter((item) => item.id !== id);
+    setCartData(updatedCart);
+    updateTotalPrice(updatedCart);
+  };
+
+  const handleOpenDialog = () => {
     setOpen(true);
   };
+
   const handleCloseDialog = () => {
-    setSelectedProduct(null);
     setOpen(false);
   };
 
-  const dispatch = useDispatch();
-  const cart = useSelector((state) => state.cart);
-  useEffect(() => {
-    Cookies.set("cart", JSON.stringify(cart), { expires: 365, secure: true });
-    const cookieCart = Cookies.get("cart");
-    console.log("cookieCart", cookieCart);
-  }, [cart]);
-
-  const deleteItem = (item) => {
-    console.log("cart", item);
-    dispatch(removeItem(item));
-  };
-  useEffect(() => {
-    setAllProducts(cart.item);
-    setTotalPrice(cart.totalAmount);
-
-    setQuantityArray(cart.item.map((item) => item.quantity)); //Inlialize quantity sates
-  }, [cart.item]);
-
-  const handleQuantityChange = (index, item, newQuantity) => {
+  const handleQuantityChange = (id, newQuantity) => {
+    debugger;
+    newQuantity = parseInt(newQuantity, 10);
     if (newQuantity >= 0) {
-      const newQuantityArray = [...quantityArray];
-      newQuantityArray[index] = newQuantity;
-      setQuantityArray(newQuantityArray);
-      dispatch(editItem({ id: item.id, quantity: newQuantity }));
+      const updatedCart = cartData.map((item) => {
+        if (item.id === id) {
+          return { ...item, quantity: newQuantity };
+        }
+        return item;
+      });
+      setCartData(updatedCart);
+      updateTotalPrice(updatedCart);
+      dispatch(editItem({ id, quantity: newQuantity }));
     }
   };
+
 
   return (
     <div>
@@ -74,14 +83,14 @@ export default function Cart() {
           <TableHead>
             <TableRow>
               <TableCell align="right">שם מוצר</TableCell>
-              <TableCell align="right"> תמונה</TableCell>
+              <TableCell align="right">תמונה</TableCell>
               <TableCell align="right">מחיר</TableCell>
               <TableCell align="right">כמות</TableCell>
               <TableCell align="right"></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {allProducts.map((row, index) => (
+            {cartData.map((row, index) => (
               <TableRow
                 key={row.id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -89,83 +98,57 @@ export default function Cart() {
                 <TableCell align="right">{row.name}</TableCell>
                 <TableCell align="right">
                   <img
-                    style={{
-                      width: "100px",
-                      height: "100px",
-                    }}
+                    style={{ width: "100px", height: "100px" }}
                     src={row.image}
                     alt=""
                   />
                 </TableCell>
                 <TableCell align="right">{row.price}</TableCell>
                 <TableCell align="right">
-                  <Button onClick={() => handleOpenDialog(row)}>
-                    {" "}
-                    <DeleteIcon color="red" />
-                  </Button>
+
+                  <TextField
+                    type="number"
+                    variant="outlined"
+                    value={row.quantity}
+                    onChange={(e) =>
+                      handleQuantityChange(row.id, e.target.value)
+                    }
+                  />
                 </TableCell>
                 <TableCell align="right">
                   <Button
-                    variant="contained"
-                    onClick={() =>
-                      handleQuantityChange(index, row, quantityArray[index] - 1)
-                    }
+                    onClick={() => {
+                      setSelectedProduct(row);
+                      handleOpenDialog();
+                    }}
                   >
-                    -
-                  </Button>
-                  <TextField
-                    id="outlined-basic"
-                    type={"number"}
-                    variant="outlined"
-                    value={quantityArray[index]}
-                    placeholder={row.quantity}
-                    onChange={(e) => handleQuantityChange(e, index, row)}
-                  />
-
-                  <Button
-                    variant="contained"
-                    onClick={() =>
-                      handleQuantityChange(index, row, quantityArray[index] + 1)
-                    }
-                  >
-                    +
+                    <DeleteIcon color="error" />
                   </Button>
                 </TableCell>
-                {selectedProduct && (
-                  <Dialog
-                    open={open}
-                    onClose={handleCloseDialog}
-                    aria-labelledby="alert-dialog-title"
-                    aria-describedby="alert-dialog-description"
-                  >
-                    <DialogTitle id="alert-dialog-title">
-                      {`האם אתה בטוח שברצונך למחוק את המוצר ${selectedProduct.name}?`}
-                    </DialogTitle>
-                    <DialogContent>
-                      <DialogContentText id="alert-dialog-description">
-
-                      </DialogContentText>
-                    </DialogContent>
-                    <DialogActions>
-                      <Button onClick={handleCloseDialog}>לא</Button>
-                      <Button
-                        onClick={() => deleteItem(selectedProduct)}
-                        autoFocus
-                      >
-                        כן
-                      </Button>
-                    </DialogActions>
-                  </Dialog>
-                )}
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-      <h3>סה"כ לתשלום : {totalPrice}</h3>
+      <h3>סה"כ לתשלום: {totalPrice}</h3>
       <Button variant="contained" to="/Pay" component={Link}>
-        תשלום{" "}
+        תשלום
       </Button>
+      <Dialog open={open} onClose={handleCloseDialog}>
+        <DialogTitle id="alert-dialog-title">האם אתה בטוח?</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            האם אתה בטוח שברצונך למחוק את המוצר?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog}>לא</Button>
+          <Button onClick={() => deleteItem(selectedProduct.id)} autoFocus>
+            כן
+          </Button>
+        </DialogActions>
+      </Dialog>
+
     </div>
   );
 }
